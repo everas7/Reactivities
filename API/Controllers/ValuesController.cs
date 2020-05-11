d2701f9dac9bb607b2faa515699c6fb83aba3160
Enmanuel Veras <enmaveras07@gmail.com>
2020-05-11 12:36:05 -0400

    Restructures webapp folders. Adds basic crud on api

--- API/Controllers/ValuesController.cs
+++ API/Controllers/ValuesController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistance;
 
-namespace DatingApp.API.Controllers
+namespace API.Controllers
 {
   [Route("api/[controller]")]
   [ApiController]
