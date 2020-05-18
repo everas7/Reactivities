73bcdeb1f5cb95ec0ba621180064780ecba17bd0
Enmanuel Veras <enmaveras07@gmail.com>
2020-05-18 11:22:25 -0400

    Adds user entity and authentication

---
+++ Application/User/CurrentUser.cs
@@ -0,0 +1,42 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Interfaces;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.User
+{
+  public class CurrentUser
+  {
+    public class Query : IRequest<User> { };
+
+    public class Handler : IRequestHandler<Query, User>
+    {
+      private readonly UserManager<AppUser> _userManager;
+      private readonly IJwtGenerator _jwtGenerator;
+      private readonly IUserAccessor _userAcessor;
+
+      public Handler(UserManager<AppUser> userManager, IJwtGenerator jwtGenerator,
+        IUserAccessor userAcessor)
+      {
+        _userAcessor = userAcessor;
+        _jwtGenerator = jwtGenerator;
+        _userManager = userManager;
+      }
+
+      public async Task<User> Handle(Query request,
+          CancellationToken cancellationToken)
+      {
+        var user = await _userManager.FindByNameAsync(_userAcessor.getCurrentUsername());
+        return new User
+        {
+          DisplayName = user.DisplayName,
+          Username = user.UserName,
+          Token = _jwtGenerator.CreateToken(user),
+          Image = null,
+        };
+      }
+    }
+  }
+}
---
+++ Application/Validators/ValidatorExtensions.cs
@@ -0,0 +1,20 @@
+using FluentValidation;
+
+namespace Application.Validators
+{
+  public static class ValidatorExtensions
+  {
+    public static IRuleBuilder<T, string> Password<T>(this IRuleBuilder<T, string>
+    ruleBuilder)
+    {
+      var options = ruleBuilder
+        .MinimumLength(6).WithMessage("Password must be at least 6 characters")
+        .Matches("[A-Z]").WithMessage("Password must contain at least 1 upper case letter")
+        .Matches("[a-z]").WithMessage("Password must contain at least 1 lower case letter")
+        .Matches("[0-9]").WithMessage("Password must contain at least a number")
+        .Matches("[^a-zA-Z0-9]").WithMessage("Password must container at least 1 non alphanumeric character");
+      
+      return options;
+    }
+  }
+}
